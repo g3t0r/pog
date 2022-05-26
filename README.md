741f91751c126e37735b222971f5378b3d3ec686
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2022-05-26 09:37:20 +0200

    added install instruction

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to install
+
+```shell
+    chmod +x ./install.sh
+    sudo ./install.sh
+
+```
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! sh
 
 chmod +x ./pog
-sudo ln -s $PWD/pog /usr/bin/pog
+ln -s $PWD/pog /usr/bin/pog
